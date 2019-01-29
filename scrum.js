f6a09c397207a895194d7bcb98eb4f6bcb3c1f5d
Jonathan Rodriguez <clubzspadez@gmail.com>
2019-01-28 22:43:27 -0500

    updating notes

---
+++ scrum.js
@@ -0,0 +1,17 @@
+//# :'######:::'######::'########::'##::::'##:'##::::'##:
+//# '##... ##:'##... ##: ##.... ##: ##:::: ##: ###::'###:
+//*  ##:::..:: ##:::..:: ##:::: ##: ##:::: ##: ####'####:
+//* . ######:: ##::::::: ########:: ##:::: ##: ## ### ##:
+//$ :..... ##: ##::::::: ##.. ##::: ##:::: ##: ##. #: ##:
+//$ '##::: ##: ##::: ##: ##::. ##:: ##:::: ##: ##:.:: ##:
+//! . ######::. ######:: ##:::. ##:. #######:: ##:::: ##:
+//! :......::::......:::..:::::..:::.......:::..:::::..::
+
+
+
+//! Purpose of the Scrum Guide
+//* Scrum is a framework for developing, delivering, and sustaining complex products. 
+//* This Guide contains the definition of Scrum. 
+//* This definition consists of Scrum’s roles, events, artifacts, and the rules that bind them together. 
+//* Ken Schwaber and Jeff Sutherland developed Scrum; the Scrum Guide is written and provided by them. 
+//* Together, they stand behind the Scrum Guide.
