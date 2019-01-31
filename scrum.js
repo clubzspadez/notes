81bd1535980b5359abc5c6547e8e9f280f6bfd93
clubzspadez <clubzspadez@gmail.com>
2019-01-30 21:32:22 -0500

    added terms for scrum. learning how each term should be applied

--- scrum.js
+++ scrum.js
@@ -15,3 +15,25 @@
 //* This definition consists of Scrum’s roles, events, artifacts, and the rules that bind them together. 
 //* Ken Schwaber and Jeff Sutherland developed Scrum; the Scrum Guide is written and provided by them. 
 //* Together, they stand behind the Scrum Guide.
+
+
+/*
+* 
+* 3 Major Types of Methodologies 
+#-> Waterfall (SDLC the most common type)
+ *   -Oldest
+ *   -Good for non-changing requirements
+#-> Spiral
+ *   -All about prototyping
+ *   -Can imbed other Methodologies
+#-> Agile
+ *   -Is incremental, focuses on short feedback cycles
+ *   -16+ different Methodologies including XP, SCRUM, Kanban & APM
+ *   -Requires most experience & integration of team 
+
+
+
+
+
+
+*/
