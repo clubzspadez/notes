f06a0165815704c56c2fcd3fdfe27190e0f4396e
clubzspadez <clubzspadez@gmail.com>
2019-01-30 22:30:05 -0500

    adding more notes from scrum book. How scrum can be good for time, development, scheduling, andimprovement

--- scrum.js
+++ scrum.js
@@ -32,8 +32,73 @@
  *   -Requires most experience & integration of team 
 
 
+ !TERMS
+
+ $ Agile
+* a set of software development practices designed to help engineers work together and adapt to changes quickly and easily.
+$ Artifact
+   * a physical or virtual tool used by a scrum team to facilitate the scrum process
+   * in this case the product backlog which helps the team identify wish list items, features, bugs, etc
+   * sprint backlog which would contain list of features,bugs, and changes
+$ Blocker
+  *  anything keeping an engineer from moving forward on a task in progress
+$ Customer
+   * whoever has engaged the team to create a product
+$ Engineer/Developer
+   * a person responsible for creating and maintaining the technology that goes into a product
+$ Engineering Organization
+   * the part of a company where engineers are employed to create and maintain products
+$ Product
+   * what the engineering organization is building or maintaining for a customer
+$ Product backlog
+   * a constantly evolving list of potential features or changes for a product
+$ Product owner
+   * a person who helps define the product for the team, and whose job may be on the line if the customer isn't satisfied
+$ Retrospective
+   * a regular opportunity for the team to reflect on how they are doing, and what they could do better
+$ Ritual
+   * a group of people coming together as part of the scrum process for a fixed time, with a specified agenda, to achieve a clearly defined outcome
+$ Scrum Master
+  *  a person responsible for maintaining the artifacts and overseeing the rituals of scrum
+$ Sprint
+  *  a fixed number of days during which the team can work together to produce an agreed upon set of changes to the product
+$ Sprint backlog
+  *  a finite and well-defined set of stories the team has agreed they can reasonably complete in the current sprint
+$ Story
+  *  a clear and consistent way of chunking, phrasing, and discussing work the team may need to do on the product
+$ User
+  *  a person who will be making use of the product
 
 
+
+  ! Time Sensitivity 
+    ? By using customer data we can integrate ideas from the team 
+    ? allowing an iterative approach that encourages learning as you go
+    # good for adapting to technologies and market changes
+
+  ! Modular Development
+   # scrum  is based on thinking in terms of slices of functionality
+   # allowing modular features to be mixed together
+   # 
+
+  ! Flexible Scheduling
+  * using sprints 
+  *
+  *
+
+  !Reflection and improvement
+  # constant improvement in the process 
+
+
+
+! WIP - Work In Progress
+
+*
+*
+*
+*
+*
+*
 
 
 */
