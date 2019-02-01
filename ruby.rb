c061f2bf126785a52a85b0922ca14e81640a05d3
Jonathan Rodriguez <clubzspadez@gmail.com>
2019-01-31 21:58:56 -0500

    working on ruby notes

---
+++ ruby.rb
@@ -0,0 +1,9 @@
+
+#! RRRRRR  UU   UU BBBBB   YY   YY 
+#! RR   RR UU   UU BB   B  YY   YY 
+#! RRRRRR  UU   UU BBBBBB   YYYYY  
+#! RR  RR  UU   UU BB   BB   YYY   
+#! RR   RR  UUUUU  BBBBBB    YYY
+puts 'hello world'
+
+puts 'okay this is working!'
