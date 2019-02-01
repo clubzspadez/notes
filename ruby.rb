c2d3dae29ab3abdab42eb48bd10ceb353209f5ad
Jonathan Rodriguez <clubzspadez@gmail.com>
2019-01-31 22:07:47 -0500

    working on understanding ruby fundamentals

--- ruby.rb
+++ ruby.rb
@@ -7,3 +7,46 @@
 puts 'hello world'
 
 puts 'okay this is working!'
+
+
+# Defining Methods in RUb
+
+def methodName(string)
+  puts 'this should print when this method is invoked'
+  puts string
+end 
+
+#? It seems we can still pass arugments to methods with paranthesis or without
+methodName('this is a string')
+
+
+
+## __          __        _    _                        _ _   _     
+## \ \        / /       | |  (_)                      (_) | | |    
+##  \ \  /\  / /__  _ __| | ___ _ __   __ _  __      ___| |_| |__  
+##   \ \/  \/ / _ \| '__| |/ / | '_ \ / _` | \ \ /\ / / | __| '_ \ 
+##    \  /\  / (_) | |  |   <| | | | | (_| |  \ V  V /| | |_| | | |
+##     \/  \/ \___/|_|  |_|\_\_|_| |_|\__, |   \_/\_/ |_|\__|_| |_|
+##                                     __/ |                       
+##                                    |___/                        
+#$   _____ _        _                 
+#$  / ____| |      (_)                
+#$ | (___ | |_ _ __ _ _ __   __ _ ___ 
+#$  \___ \| __| '__| | '_ \ / _` / __|
+#$  ____) | |_| |  | | | | | (_| \__ \
+#$ |_____/ \__|_|  |_|_| |_|\__, |___/
+#$                           __/ |    
+#$                          |___/     
+
+
+#!String Concatenation
+
+#!String Interpolation
+
+#!Methods
+
+#!Common methods
+
+#!Variable Assignment
+
+#!Escaping
