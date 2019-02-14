750444d42b2d1ace6f0ed6fe568d1f5945c7bdb9
Jonathan Rodriguez <clubzspadez@gmail.com>
2019-02-13 22:29:58 -0500

    working on learning graphql

---
+++ hackernews_clone/src/index.js
@@ -0,0 +1,24 @@
+// import Server functon/object
+const { GraphQLServer } = require('graphql-yoga')
+
+// create definition types
+
+const typeDefs = `
+type Query {
+  info: String!
+}
+`
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
