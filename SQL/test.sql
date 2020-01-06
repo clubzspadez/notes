56db4aba378fdc75386aa62b28df23c568afc841
clubzspadez <clubzspadez@gmail.com>
2020-01-05 20:49:54 -0500

    adding notes for ruby

--- SQL/test.sql
+++ SQL/test.sql
@@ -1,35 +1,117 @@
--- DROP DATABASE IF EXISTS book_shop;
--- CREATE DATABASE book_shop;
--- USE book_shop;
--- SELECT book_shop;
-DROP TABLE IF EXISTS books;
+-- -- DROP DATABASE IF EXISTS book_shop;
+-- -- CREATE DATABASE book_shop;
+-- -- USE book_shop;
+-- -- SELECT book_shop;
+-- DROP TABLE IF EXISTS books;
+--
+-- CREATE TABLE books
+-- (
+-- book_id SERIAL NOT NULL PRIMARY KEY,
+-- title VARCHAR(100),
+-- author_fname VARCHAR(100),
+-- author_lname VARCHAR(100),
+-- released_year INT,
+-- stock_quantity INT,
+-- pages INT
+-- );
+--
+-- INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
+-- VALUES
+-- ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+-- ('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+-- ('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+-- ('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+-- ('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+-- ('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+-- ('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+-- ('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+-- ('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+-- ('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+-- ('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+-- ('Where I''m Calling From: Selected Stories', 'Raymond', 'Carver', 1989, 12, 526),
+-- ('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+-- ('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+-- ('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+-- ('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
 
-CREATE TABLE books
-(
-book_id SERIAL NOT NULL PRIMARY KEY,
-title VARCHAR(100),
-author_fname VARCHAR(100),
-author_lname VARCHAR(100),
-released_year INT,
-stock_quantity INT,
-pages INT
-);
-
-INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages)
-VALUES
-('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
-('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
-('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
-('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
-('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
-('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
-('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
-('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
-('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
-('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
-('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
-('Where I''m Calling From: Selected Stories', 'Raymond', 'Carver', 1989, 12, 526),
-('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
-('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
-('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
-('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+-- INSERT INTO payout_file_runs("transactionDate", "transactionId", "adaptEvent", sponsor, "currencyIds")
+-- VALUES ('2019-10-16 12:19:07.444261-04', '2d0ec5b9-b006-4fd5-92f6-fd1898a4af3c', 'jrodtesting', '71bb76d3-dae9-41da-a36d-8bc7da5126b9', '{2b3138a6-d39e-43e5-9c61-75c656c3f378,6026b6d1-add9-4ee8-93ea-cecb9a0960ce}');
+-- DROP TABLE IF EXISTS temporary_payout;
+-- CREATE TEMP TABLE temporary_payout("transactionDate" timestamp with time zone,
+-- "orderId" VARCHAR,
+-- "transactionId" uuid,
+-- "adaptEvent" VARCHAR,
+-- status VARCHAR,
+-- sponsor uuid,
+-- currency uuid,
+-- "platformId" character varying,
+-- "teamId" character varying,
+-- grantsTotal bigint,
+-- "appliedPayoutThreshold" integer [],
+-- "currencyType" currency_type,
+-- "perDollarConversion" integer,
+-- "totalEarnedPayouts" bigint,
+-- "purchasesTotal" bigint,
+-- "owedPayout" bigint) ON COMMIT DROP;
+SELECT v_transaction_date AS "transactionDate",
+              v_payout_type AS "orderId",
+              v_transaction_id AS "transactionId",
+              v_adapt_event AS "adaptEvent",
+              v_status AS status,
+              grants_thresholds.sponsor,
+              grants_thresholds.currency,
+              grants_thresholds."platformId",
+              grants_thresholds."teamId",
+              grants_thresholds."grantsTotal",
+              grants_thresholds."appliedPayoutThresholds",
+              grants_thresholds.currency_type,
+              grants_thresholds."perDollarConversion",
+              grants_thresholds."totalEarnedPayouts",
+              "purchasesSums"."purchasesTotal",
+              CASE
+                WHEN "purchasesSums"."purchasesTotal" IS NULL THEN grants_thresholds."totalEarnedPayouts"
+                ELSE (grants_thresholds."totalEarnedPayouts" - "purchasesSums"."purchasesTotal")
+              END AS "owedPayout"
+        --grants_thresholds: apply thresholds to sum of earned grants and return the greatest threshold that has been met
+        FROM  (SELECT thresholds.sponsor, thresholds.currency, thresholds."platformId", thresholds."teamId", thresholds."grantsTotal", thresholds."appliedPayoutThresholds", thresholds.currency_type, thresholds."perDollarConversion",
+               CASE
+                 WHEN thresholds."appliedPayoutThresholds" IS NULL THEN thresholds."grantsTotal"
+                 WHEN thresholds."grantsTotal" >= thresholds."appliedPayoutThresholds" [ 5 ] THEN thresholds."appliedPayoutThresholds" [ 5 ]
+                 WHEN thresholds."grantsTotal" >= thresholds."appliedPayoutThresholds" [ 4 ] THEN thresholds."appliedPayoutThresholds" [ 4 ]
+                 WHEN thresholds."grantsTotal" >= thresholds."appliedPayoutThresholds" [ 3 ] THEN thresholds."appliedPayoutThresholds" [ 3 ]
+                 WHEN thresholds."grantsTotal" >= thresholds."appliedPayoutThresholds" [ 2 ] THEN thresholds."appliedPayoutThresholds" [ 2 ]
+                 WHEN thresholds."grantsTotal" >= thresholds."appliedPayoutThresholds" [ 1 ] THEN thresholds."appliedPayoutThresholds" [ 1 ]
+                 ELSE 0
+               END AS "totalEarnedPayouts"
+                --thresholds: join grantsSum with currencies to pull the correct set of thresholds (based on whether part of a family)
+                FROM  (SELECT grants_sums.sponsor, grants_sums.currency, grants_sums."platformId", grants_sums."teamId", grants_sums."grantsTotal",
+                              CASE
+                                WHEN grants_sums."teamId" != NULL AND currencies."familyRewardsEnabled" IS TRUE THEN currencies."familyPayoutThresholds"
+                                ELSE currencies."payoutThresholds"
+                              END AS "appliedPayoutThresholds",
+                              currencies.type AS currency_type,
+                              currencies."perDollar" AS "perDollarConversion"
+                       -- grants_sum: sum of all grants per currency and payee (as platform id as referred by purchases table and return to adapt)
+                       FROM (SELECT grants_payee.sponsor, grants_payee.currency, grants_payee.adjusted_payee AS "platformId", grants_payee."teamId",
+                                    SUM(CAST(detail->>'amount' AS INTEGER)) AS "grantsTotal"
+                             -- grants_payee: substitute platformId as payee if no payee is specified (payee is new field for family rewards, need to run migration to pull over platform id for all previous data
+                             FROM (SELECT grants.sponsor, grants.currency, "teamId", grants.detail,
+                                          CASE
+                                            WHEN payee IS NULL THEN grants."platformId"
+                                            ELSE payee
+                                          END AS adjusted_payee
+                                   FROM grants
+                                   WHERE grants.currency = ANY(v_currency_ids)
+                                   AND grants.status = 'complete'
+                                   AND grants."platformId" IS NOT NULL) AS grants_payee
+                             GROUP BY grants_payee.sponsor, grants_payee.currency, grants_payee.adjusted_payee, grants_payee."teamId") AS grants_sums
+                LEFT JOIN currencies ON currencies.id = grants_sums.currency) AS thresholds)
+        AS grants_thresholds
+        LEFT JOIN (SELECT purchases.currency, purchases."platformId", "teamId", SUM(purchases.amount) AS "purchasesTotal"
+                   FROM purchases
+                   WHERE purchases.currency = ANY(v_currency_ids)
+                   AND purchases.status = 'complete'
+                   AND purchases."platformId" IS NOT NULL
+                   GROUP BY purchases.currency, purchases."platformId", purchases."teamId") AS "purchasesSums"
+        ON grants_thresholds.currency = "purchasesSums".currency
+        AND grants_thresholds."platformId" = "purchasesSums"."platformId" AND (grants_thresholds."teamId" = "purchasesSums"."teamId" OR (grants_thresholds."teamId" IS NULL AND "purchasesSums"."teamId" IS NULL))
--- ruby/ruby.rb
+++ ruby/ruby.rb
@@ -1,9 +1,10 @@
 
-#! RRRRRR  UU   UU BBBBB   YY   YY 
-#! RR   RR UU   UU BB   B  YY   YY 
-#! RRRRRR  UU   UU BBBBBB   YYYYY  
-#! RR  RR  UU   UU BB   BB   YYY   
+#! RRRRRR  UU   UU BBBBB   YY   YY
+#! RR   RR UU   UU BB   B  YY   YY
+#! RRRRRR  UU   UU BBBBBB   YYYYY
+#! RR  RR  UU   UU BB   BB   YYY
 #! RR   RR  UUUUU  BBBBBB    YYY
+#  Ruby documentation and classes/methods -> https://ruby-doc.com/
 puts 'hello world'
 
 puts 'okay this is working!'
@@ -12,29 +13,29 @@ puts 'okay this is working!'
 def methodName(string)
   puts 'this should print when this method is invoked'
   puts string
-end 
+end
 
 #? It seems we can still pass arugments to methods with paranthesis or without
 methodName('this is a string')
 
 
 
-## __          __        _    _                        _ _   _     
-## \ \        / /       | |  (_)                      (_) | | |    
-##  \ \  /\  / /__  _ __| | ___ _ __   __ _  __      ___| |_| |__  
-##   \ \/  \/ / _ \| '__| |/ / | '_ \ / _` | \ \ /\ / / | __| '_ \ 
+## __          __        _    _                        _ _   _
+## \ \        / /       | |  (_)                      (_) | | |
+##  \ \  /\  / /__  _ __| | ___ _ __   __ _  __      ___| |_| |__
+##   \ \/  \/ / _ \| '__| |/ / | '_ \ / _` | \ \ /\ / / | __| '_ \
 ##    \  /\  / (_) | |  |   <| | | | | (_| |  \ V  V /| | |_| | | |
 ##     \/  \/ \___/|_|  |_|\_\_|_| |_|\__, |   \_/\_/ |_|\__|_| |_|
-##                                     __/ |                       
-##                                    |___/                        
-#$   _____ _        _                 
-#$  / ____| |      (_)                
-#$ | (___ | |_ _ __ _ _ __   __ _ ___ 
+##                                     __/ |
+##                                    |___/
+#$   _____ _        _
+#$  / ____| |      (_)
+#$ | (___ | |_ _ __ _ _ __   __ _ ___
 #$  \___ \| __| '__| | '_ \ / _` / __|
 #$  ____) | |_| |  | | | | | (_| \__ \
 #$ |_____/ \__|_|  |_|_| |_|\__, |___/
-#$                           __/ |    
-#$                          |___/     
+#$                           __/ |
+#$                          |___/
 
 
 #!String Concatenation
@@ -44,14 +45,20 @@ p sentence
 sentence2 = ' and this is my additional sentence'
 
 puts sentence + sentence2
- 
-#!String Interpolation
 
+#!String Interpolation
+"#{sentence}"
 #!Methods
 # * Common methods in RUBY:
 # :nesting, :used_modules, :constants, :new, :allocate, :superclass, :<=>, :<=, :>=, :==, :===, :autoload?, :autoload, :included_modules, :include?, :name, :ancestors, :attr, :attr_reader, :attr_writer, :attr_accessor, :instance_methods, :public_instance_methods, :protected_instance_methods, :private_instance_methods, :const_get, :const_set, :const_defined?, :class_variables, :remove_class_variable, :class_variable_get, :class_variable_set, :class_variable_defined?, :public_constant, :freeze, :inspect, :deprecate_constant, :private_constant, :const_missing, :singleton_class?, :prepend, :class_exec, :module_eval, :class_eval, :include, :<, :>, :remove_method, :undef_method, :alias_method, :protected_method_defined?, :module_exec, :method_defined?, :public_method_defined?, :to_s, :public_class_method, :public_instance_method, :define_method, :private_method_defined?, :private_class_method, :instance_method, :instance_variable_set, :instance_variable_defined?, :remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :tap, :instance_variable_get, :instance_variables, :method, :public_method, :singleton_method, :define_singleton_method, :public_send, :extend, :to_enum, :enum_for, :pp, :=~, :!~, :eql?, :respond_to?, :object_id, :send, :display, :nil?, :hash, :class, :singleton_class, :clone, :dup, :itself, :yield_self, :taint, :tainted?, :untrust, :untaint, :trust, :untrusted?, :methods, :frozen?, :protected_methods, :singleton_methods, :public_methods, :private_methods, :!, :equal?, :instance_eval, :instance_exec, :!=, :__send__, :__id__]
 
 #!Common methods
+#
+#
+#
+# Symbols -> :symbol
+# - Symbols are light weight strings that can be used to reference strings
+#
 
 #!Variable Assignment
 # * Variables are Pass-by-Value:
@@ -69,19 +76,33 @@ a = "I have changed"
 #  while b still points to the old address for the string "Ruby"
 #
 #
-#!Escapin
 #
 
 
 
-#Assignment for checking input Ruby
+# Methods
+# In ruby the last evaluated statement in the method is implicitly returned
+# Execution flow: Methods cannot be invoked if they have not been defined first.
+# Example:
+#
+# test_method(input_1, input_2)
+#
+#
+# def test_method(in1, in2)
+#   in1 + in2
+# end
+#
+# ^ this will toss an error where test_method is being invoked as the execution flow does not create and then execute.
+# - You will need to define your methods before hand.
+#
 #
 def getFullName
 	puts "What is your first name?"
 	firstName = gets.chomp
 	puts "What is your last name?"
 	lastName = gets.chomp
-	return printName(firstName, lastName)
+  # this will return the method below as it is the last evaluated statement
+  printName(firstName, lastName)
 end
 
 def printName(first, last)
@@ -91,19 +112,93 @@ def printName(first, last)
 	puts "Your name has #{first.length + last.length} characters in it"
 end
 
-getFullName
-
-#update
 #
-#Object Oriented Programming 
+# BRANCHING -> If/elsif/else/end
+#
+# if condition
+# -> do something
+# else
+# -> do something
+# end
+#
+# if condition
+#  -> do something
+# elsif
+#  -> do something
+# else
+#  -> do something
+# end
+#
+#
+#                                          _______ _                 _
+#      /\                                 / /_   _| |               | |
+#     /  \   _ __ _ __ __ _ _   _ ___    / /  | | | |_ ___ _ __ __ _| |_ ___  _ __ ___
+#    / /\ \ | '__| '__/ _` | | | / __|  / /   | | | __/ _ \ '__/ _` | __/ _ \| '__/ __|
+#   / ____ \| |  | | | (_| | |_| \__ \ / /   _| |_| ||  __/ | | (_| | || (_) | |  \__ \
+#  /_/    \_\_|  |_|  \__,_|\__, |___/ _/   |_____|\__\___|_|  \__,_|\__\___/|_|  |___/
+#                           __/ |
+#                          |___/
+# array methods can be found -> https://ruby-doc.org/core-2.4.1/Array.html
+# range( represents an interval 1..100) methods can be found ->
+# Arrays can be defined or created using the literal constructor []
+# Arrays can also be defined by the following:
+# ary = Array.new    #=> []
+#
+#
+# Common iterators when working with ruby
+# -> Each Iterator. This iterator returns all the elements of an array or a hash. Each iterator returns each value one by one.
+#   collection.each do |variable_name|
+#    # code to be iterate
+#   end
+#
+# -> Collect Iterator. This iterator returns all the elements of a collection.
+#  The collect iterator returns an entire collection, regardless of whether it is an array or hash
+#   Collection = collection.collect{ |variables in scope| (code to iterate)}
+#
+# -> Times Iterator.  In this iterator, a loop is implanted with the specific number of time.
+#  The loop is initially started from zero and runs until the one less than the specified number.
+#  This can be used with no iteration variable.. We can add an iteration variable by using the vertical bars around the identifier.
+#
+# -> Upto Iterator. This iterator follows top to bottom approach. It includes both the top and bottom variable in the iteration.
+#
+# -> Downto Iterator. This iterator follows bottom to top approach. It includes both the top and bottom variable in the iteration.
+# -> Step Iterator. Ruby step iterator is used to iterate where the user has to skip a specified range
+# -> Each_Line Iterator. Ruby each_line iterator is used to iterate over a new line in the string
+#
+#
+#
+#  HASH or HASHES  -> { 'a' => 1, 'b' => 2}
+#  HASH Class documentation -> https://ruby-doc.com/core-2.5.2/Hash.html
+# You can access values by referencing the key you want to access
+#
+# example:
+# with strings
+# sample_hash = { 'a' => 1, 'b' => 2}
+# sample_hash['b'] =>  2
+#
+# with symbols
+# sample_hash = { a: 1, b: 2}
+# sample_hash[:b] =>  2
+#
+#
+# Each named key can be strings or symbols
+# And a hash can be created with
+#  -> Hash.new
+#
+
+
+
+
+#
+# Object Oriented Programming
 #
 #
 class Car
- #attr_accessor: implements both setter and getter for overwriting values or setting
+ #iimplements both setter and getter for overwriting values or setting
  #attr_writer: implements setter
  #attr_reader: implements getter
  attr_accessor :numberOfWheels
- 
+
  #constructor
  def initialize(numberOfWheels, tankType, seatingCapacity, maxVelocity)
   @numberOfWheels = numberOfWheels
@@ -111,10 +206,10 @@ class Car
   @seatingCapacity = seatingCapacity
   @maxVelocity = maxVelociy
  end
- 
+
  #methods can be added to this class
- 
- def methodOne 
+
+ def methodOne
   #we can access the method from the instance of the car, ie: lambo.methodOne
  end
 
@@ -125,6 +220,6 @@ end
 lambo = Vehicle.new(4, 'Gas', 2, 250)
 #
 lambo.numberOfWheels # => 4
-lambo.numberOfWheels = 6 
+lambo.numberOfWheels = 6
 lambo.numberOfWheels # => 6
 #
